7df1181695be964869c672c19e322f0a1027cfcb
Bastien <b.sante@nouvellesimages.xyz>
2024-11-20 11:40:22 +0100

    Created unity project

---
+++ Assets/Local/Bastien/Scripts/Clawster/Controller.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*This code was copied straight from the video just to get the script working
+ * We need to rework it ASAP to adapt it to a touch screen, and remove some
+ * of the horrid tecnhiques used to make movement possible*/
+public class Controller : MonoBehaviour
+{
+    public Rigidbody controller;
+    public Transform camera;
+
+    public float speed = 6;
+    private Vector3 _velocity;
+
+    public float turnSmoothVelocity;
+    public float turnSmoothTime = 0.1f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+       float horizontal = Input.GetAxis("Horizontal");
+       float vertical = Input.GetAxis("Vertical");
+       Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+       if (direction.magnitude >= 0.1f && controller.velocity.magnitude < 8)
+       {
+           float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
+           
+           Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+           controller.AddForce(moveDir.normalized * speed * Time.deltaTime);
+       }
+    }
+}
---
+++ Assets/Local/Bastien/Scripts/Clawster/CrabController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrabController : MonoBehaviour
+{
+    /*Slightly modified version of the script,
+    in order to clean some of the terrible variable
+    names, and make the thing more digestable overall*/
+
+    [Header("GameObjects")] 
+    public GameObject Crab;             //Clawster himself!
+    public GameObject[] legTargets;     //Target positions of the IK chain
+    public GameObject[] legCubes;       //Cube objects corresponing to above targets
+
+    [Header("Movement & Rotation")]
+    public bool enableBodyRotation = false;
+    public bool enableMovementRotation = false;
+    public bool rigidBodyController;
+
+    [Header("Leg Motion")]
+    public float moveDistance = 0.7f;       //Maximum distance between 2 steps
+    public float stepHeight = 0.2f;         //Maximum height that can be climbed
+    public float jitterCutoff = 0f;         //I have no idea
+    public float legSmoothing = 8f;         //I have no idea for the moment
+    public float bodySmoothing = 8f;        //I have no idea for the moment
+    public float overStepFac = 4f;          //I have no idea for the moment
+
+    //Private garbage (I showed it to fread, IHO it's disgusting)
+    private Vector3 _velocity;
+    private Vector3 _lastVelocity = Vector3.one;
+    private Vector3 _lastCrabPos;
+    private Vector3 _lastBodyUp;
+    private Vector3[] _currLegPos;
+    private Vector3[] _origLegPos;
+    
+    private List<int> _nextMoveInd = new List<int>();
+    private List<int> _currMoveInd = new List<int>();
+    private List<int> _oppoMoveInd = new List<int>();
+
+    private bool _currLeg = true;
+    private float _resetTimer = 0.5f;
+
+    private void Start()
+    {
+        _lastBodyUp = transform.up;
+        
+        _currLegPos = new Vector3[legTargets.Length]; //Making sure both arrays have the same size
+        _origLegPos = new Vector3[legTargets.Length]; //Repeat for other array
+
+        for (int i = 0; i < legTargets.Length; i++)
+        {
+            _currLegPos[i] = legTargets[i].transform.position; //Assign pos from the target
+            _currLegPos[i] = legTargets[i].transform.position; //Make values identical in both arrays
+
+            if (_currLeg)
+            {
+                _oppoMoveInd.Add(i + 1);
+                _currLeg = false;
+            }
+            else if (!_currLeg)
+            {
+                _oppoMoveInd.Add(i - 1);
+                _currLeg = true;
+            }
+            
+            _lastCrabPos = Crab.transform.position;
+
+            rotateBody();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _velocity = Crab.transform.position - _lastCrabPos; //Set velocity to the delta between the positions
+        _velocity = (_velocity + bodySmoothing * _lastVelocity) / (bodySmoothing + 1f);
+    }
+}
+
