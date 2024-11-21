d4513b101ba9efce6a3d1ad01761e8a372423c41
Bastien <b.sante@nouvellesimages.xyz>
2024-11-21 17:05:14 +0100

    Added joystick-based movement for mobile
    Thanks to YouTube tutorials, I found a basic way to do it with the new InputSystem
    //TODO : Add the functionality for the joystick to "teleport" tot the press location, a la Brawl Stars

--- Assets/Local/Bastien/Scripts/Clawster/Controller.cs
+++ Assets/Local/Bastien/Scripts/Clawster/Controller.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 /*This code was copied straight from the video just to get the script working
  * We need to rework it ASAP to adapt it to a touch screen, and remove some
@@ -9,28 +10,33 @@ public class Controller : MonoBehaviour
 {
     public Rigidbody controller;
     public Transform camera;
+    public PlayerInput playerInput;
     public float velocityCap;
     
     public float speed = 6;
+    private Vector2 _movement;
     private Vector3 _velocity;
-    
 
     public float turnSmoothVelocity;
     public float turnSmoothTime = 0.1f;
+
+    public void InputPlayer(InputAction.CallbackContext context)
+    {
+        _movement = context.ReadValue<Vector2>();
+    }
     
     // Update is called once per frame
     void Update()
     {
-       float horizontal = Input.GetAxis("Horizontal");
-       float vertical = Input.GetAxis("Vertical");
-       Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        
+       Vector3 direction = new Vector3(_movement.x, 0f, _movement.y).normalized;
 
        if (direction.magnitude >= 0.1f && controller.velocity.magnitude < 4)
        {
            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
            
            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-           controller.AddForce(moveDir.normalized * speed * Time.deltaTime, ForceMode.Acceleration);
+           controller.AddForce(direction * speed * Time.deltaTime, ForceMode.Acceleration);
        }
     }
 }
