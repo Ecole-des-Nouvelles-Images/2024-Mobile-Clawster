51f1d9a7beda655f39adb70e2f4028f39e0aed58
NoahMil <noah.miliani13@gmail.com>
2024-11-26 14:43:25 +0100

    Adding Fading Border

--- Assets/Local/Noah/Scripts/BeachMeshGenerator.cs
+++ Assets/Local/Noah/Scripts/BeachMeshGenerator.cs
@@ -11,43 +11,52 @@ namespace Local.Noah.Scripts
         Bot,
         Left,
         Right,
-        All
     }
 
     public class BeachMeshGenerator : MonoBehaviour
     {
-        [Space(5), Header("Grid")]
-        [SerializeField]  private CellVisual _prefabsCell;
+        [Space(5), Header("Grid")] [SerializeField]
+        private CellVisual _prefabsCell;
+
         public Vector2Int GridSize;
         [SerializeField] private float _maxHeight = 10;
-    
-        [Space(5), Header("PerlinNoise")]
-        [SerializeField] private bool _doPerlinNoiseInRunTime = true;
+        [SerializeField] private int _seed;
+
+        [Space(5), Header("PerlinNoise")] [SerializeField]
+        private bool _doPerlinNoiseInRunTime = true;
+
+        [SerializeField, Range(0, 1)] private float _perlinMaxValue = 1;
+
         [SerializeField] private Vector2 _perlinNoiseOffset;
         [SerializeField] private float _perlinNoiseScale;
+        [SerializeField] private int _octaves = 1;
+        [SerializeField] private float _lacunarity = 1f;
 
-        [Space(5), Header("Borders")] 
-        [SerializeField] private bool _doBorders;
+        [Space(5), Header("Borders")] [SerializeField]
+        private bool _doBorders;
+
+        [SerializeField] private bool _doFading;
         [SerializeField] private Direction _borderDirection;
         [SerializeField] private int _borderSize = 1;
-        [SerializeField, Range(0, 1)] private float _borderMaxValue;
-        [SerializeField] private bool _doFading;
-        [SerializeField] private AnimationCurve _fadingCurve = AnimationCurve.EaseInOut(0,0,1,1);
-        
-        [Space(5), Header("Fade")]
-        [SerializeField] private bool _doFade;
+        [SerializeField, Range(0, 1)] private float _borderMaxValue = 1;
+
+        [Space(5), Header("Fade")] [SerializeField]
+        private bool _doFade;
+
         [SerializeField, Range(0, 1)] private float _fadeValue;
-        [SerializeField] private AnimationCurve _fadeAnimationCurve = AnimationCurve.EaseInOut(0,0,1,1);
-        
-        [Space(5), Header("Do Thresholds")]
-        [SerializeField] private bool _doDoThresholds;
+        [SerializeField] private AnimationCurve _fadeAnimationCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
+
+        [Space(5), Header("Do Thresholds")] [SerializeField]
+        private bool _doDoThresholds;
+
         [SerializeField, Range(0, 1)] private float _thresholdValue = 0.5f;
-        
-        
+
+
         private CellData[,] _grid;
         private MeshFilter _meshFilter;
 
-        private void Awake() {
+        private void Awake()
+        {
             _meshFilter = GetComponent<MeshFilter>();
         }
 
@@ -56,163 +65,270 @@ namespace Local.Noah.Scripts
             Initialize();
         }
 
-        void Update() {
-            if (_doPerlinNoiseInRunTime) {
+        void Update()
+        {
+            if (_doPerlinNoiseInRunTime)
+            {
                 DoPerlinNoise();
             }
 
-            if (_doBorders) {
+            if (_doBorders)
+            {
                 DoBorder(_borderDirection);
             }
-            if(_doFade)DoFade();
-        
-            if(_doDoThresholds)DoThreshold();
-        
+
+            if (_doFade) DoFade();
+
+            if (_doDoThresholds) DoThreshold();
             RecolorGrid();
         }
 
         [ContextMenu("Process")]
-        public void Process() {
+        public void Process()
+        {
             Initialize();
             DoPerlinNoise();
             Populate();
         }
-        
+
         [ContextMenu("Generate Mesh")]
-        public void GenerateProceduralMesh() {
+        public void GenerateProceduralMesh()
+        {
             GenerateMeshFromGrid();
         }
-        
-        private void Initialize() {
+
+        private void Initialize()
+        {
             _grid = new CellData[GridSize.x, GridSize.y];
-            for (int x = 0; x < GridSize.x; x++) {
-                for (int y = 0; y < GridSize.y; y++) {
+            for (int x = 0; x < GridSize.x; x++)
+            {
+                for (int y = 0; y < GridSize.y; y++)
+                {
                     _grid[x, y] = new CellData(x, y);
                 }
             }
         }
 
-        private void DoPerlinNoise() {
-            for (int x = 0; x < GridSize.x; x++) {
-                for (int y = 0; y < GridSize.y; y++) {
-                    _grid[x, y] .height = Mathf.PerlinNoise(
-                        x*_perlinNoiseScale +_perlinNoiseOffset.x,
-                        y*_perlinNoiseScale +_perlinNoiseOffset.y);
+        private void DoPerlinNoise()
+        {
+            System.Random random = new System.Random(_seed);
+            Vector2[] octaveOffsets = new Vector2[_octaves];
+
+            for (int i = 0; i < _octaves; i++)
+            {
+                octaveOffsets[i] = new Vector2(random.Next(-100000, 100000), random.Next(-100000, 100000));
+            }
+
+            for (int x = 0; x < GridSize.x; x++)
+            {
+                for (int y = 0; y < GridSize.y; y++)
+                {
+                    float noiseValue = 0f;
+                    float amplitude = 1f;
+                    float frequency = 1f;
+                    float maxAmplitude = 0f;
+
+                    for (int i = 0; i < _octaves; i++)
+                    {
+                        float sampleX = (x * frequency * _perlinNoiseScale) + _perlinNoiseOffset.x + octaveOffsets[i].x;
+                        float sampleY = (y * frequency * _perlinNoiseScale) + _perlinNoiseOffset.y + octaveOffsets[i].y;
+
+                        noiseValue += Mathf.PerlinNoise(sampleX, sampleY) * amplitude;
+
+                        maxAmplitude += amplitude;
+                        amplitude *= 0.5f;
+                        frequency *= _lacunarity;
+                    }
+
+                    noiseValue /= maxAmplitude;
+                    noiseValue = Mathf.Clamp(noiseValue, 0f, 1f);
+
+                    _grid[x, y].height = noiseValue * _perlinMaxValue;
                     _grid[x, y].perlinNoiseheight = _grid[x, y].height;
                 }
             }
         }
-    
-        private void DoBorder(Direction direction) {
-            for (int x = 0; x < GridSize.x; x++) {
-                for (int y = 0; y < GridSize.y; y++) {
+
+        private void DoBorder(Direction direction)
+        {
+            int bufferZone = _borderSize / 2;
+
+            for (int x = 0; x < GridSize.x; x++)
+            {
+                for (int y = 0; y < GridSize.y; y++)
+                {
+                    float fadeFactor = 0;
+                    bool isBorder = false;
+
                     switch (direction)
                     {
                         case Direction.Top:
-                            if (y < _borderSize) { 
-                                float fadeFactor = (float)y / _borderSize;
-                                _grid[x, y].height = Mathf.Lerp(_grid[x, y].perlinNoiseheight, _borderMaxValue, _fadingCurve.Evaluate(fadeFactor)); 
+                            if (y < _borderSize)
+                            {
+                                isBorder = true;
+                                if (_doFading)
+                                {
+                                    fadeFactor = CalculateFadeFactor(y, bufferZone, _borderSize);
+                                }
+                                else
+                                {
+                                    _grid[x, y].height = _borderMaxValue;
+                                }
                             }
+
                             break;
                         case Direction.Bot:
-                            if (y > GridSize.y - _borderSize - 1) {
-                                _grid[x, y].height = _borderMaxValue;
+                            if (y >= GridSize.y - _borderSize)
+                            {
+                                isBorder = true;
+                                if (_doFading)
+                                {
+                                    fadeFactor = CalculateFadeFactor(GridSize.y - 1 - y, bufferZone, _borderSize);
+                                }
+                                else
+                                {
+                                    _grid[x, y].height = _borderMaxValue;
+                                }
                             }
-                            break;
 
+                            break;
                         case Direction.Left:
-                            if (x < _borderSize) {
-                                _grid[x, y].height = _borderMaxValue;
+                            if (x < _borderSize)
+                            {
+                                isBorder = true;
+                                if (_doFading)
+                                {
+                                    fadeFactor = CalculateFadeFactor(x, bufferZone, _borderSize);
+                                }
+                                else
+                                {
+                                    _grid[x, y].height = _borderMaxValue;
+                                }
                             }
-                            break;
 
+                            break;
                         case Direction.Right:
-                            if (x > GridSize.x - _borderSize - 1) {
-                                _grid[x, y].height = _borderMaxValue;
+                            if (x >= GridSize.x - _borderSize)
+                            {
+                                isBorder = true;
+                                if (_doFading)
+                                {
+                                    fadeFactor = CalculateFadeFactor(GridSize.x - 1 - x, bufferZone, _borderSize);
+                                }
+                                else
+                                {
+                                    _grid[x, y].height = _borderMaxValue;
+                                }
                             }
-                            break;
 
-                        case Direction.All:
-                            if ((x < _borderSize || x > GridSize.x - _borderSize - 1) || (y < _borderSize || y > GridSize.y - _borderSize - 1)) {
-                                _grid[x, y].height = _borderMaxValue;
-                            }
                             break;
+                    }
+
+                    if (isBorder && _doFading)
+                    {
+                        _grid[x, y].height = Mathf.Lerp(_borderMaxValue, _grid[x, y].perlinNoiseheight, fadeFactor);
                     }
                 }
             }
         }
-        
-        private void DoFade() {
-            for (int x = 0; x < GridSize.x; x++) {
-                for (int y = 0; y < GridSize.y; y++) {
+
+
+        private float CalculateFadeFactor(int distance, int bufferZone, int borderSize)
+        {
+            if (distance < bufferZone)
+            {
+                return 0;
+            }
+            else
+            {
+                return (float)(distance - bufferZone) / (borderSize - bufferZone);
+            }
+        }
+
+
+        private void DoFade()
+        {
+            for (int x = 0; x < GridSize.x; x++)
+            {
+                for (int y = 0; y < GridSize.y; y++)
+                {
                     float time = (float)y / GridSize.y;
-                    _grid[x, y].height = Mathf.Lerp(_grid[x, y].perlinNoiseheight, _fadeValue, _fadeAnimationCurve.Evaluate(time)); 
+                    _grid[x, y].height = Mathf.Lerp(_grid[x, y].perlinNoiseheight, _fadeValue,
+                        _fadeAnimationCurve.Evaluate(time));
                 }
             }
         }
-    
-        private void DoThreshold() {
-            for (int x = 0; x < GridSize.x; x++) {
-                for (int y = 0; y < GridSize.y; y++) {
-                    if (_grid[x, y].height > _thresholdValue) {
+
+        private void DoThreshold()
+        {
+            for (int x = 0; x < GridSize.x; x++)
+            {
+                for (int y = 0; y < GridSize.y; y++)
+                {
+                    if (_grid[x, y].height > _thresholdValue)
+                    {
                         _grid[x, y].height = 1;
                     }
-                    else {
+                    else
+                    {
                         _grid[x, y].height = 0;
                     }
                 }
             }
         }
-    
-        private void RecolorGrid() {
-            for (int x = 0; x < GridSize.x; x++) {
-                for (int y = 0; y < GridSize.y; y++) {
+
+        private void RecolorGrid()
+        {
+            for (int x = 0; x < GridSize.x; x++)
+            {
+                for (int y = 0; y < GridSize.y; y++)
+                {
                     _grid[x, y].visual.SetColorByHeight(_grid[x, y].height);
                 }
             }
         }
-    
 
-        private void Populate() {
-            for (int x = 0; x < GridSize.x; x++) {
-                for (int y = 0; y < GridSize.y; y++) {
-                    CellVisual visual =Instantiate(_prefabsCell, new Vector3(x, 0, y), quaternion.identity);
+
+        private void Populate()
+        {
+            for (int x = 0; x < GridSize.x; x++)
+            {
+                for (int y = 0; y < GridSize.y; y++)
+                {
+                    CellVisual visual = Instantiate(_prefabsCell, new Vector3(x, 0, y), quaternion.identity);
                     visual.transform.SetParent(transform);
-                
+
                     _grid[x, y].visual = visual;
                     visual.SetColorByHeight(_grid[x, y].height);
                 }
             }
         }
-        
-        
-        private void GenerateMeshFromGrid() {
-            // Vérifie si la height map est générée
-            if (_grid == null || _grid.Length == 0) {
-                Debug.LogError("Height map non générée. Assurez-vous d'appeler PerlinNoise1() avant de générer le mesh.");
-                return;
-            }
 
+
+        private void GenerateMeshFromGrid()
+        {
             int width = GridSize.x;
             int height = GridSize.y;
 
-            // Crée un tableau de sommets et de triangles
             Vector3[] vertices = new Vector3[(width + 1) * (height + 1)];
             int[] triangles = new int[width * height * 6];
 
-            // Remplit le tableau des sommets
-            for (int y = 0; y <= height; y++) {
-                for (int x = 0; x <= width; x++) {
-                    float heightValue = _grid[x % width, y % height].height; // Hauteur depuis la height map
-                    vertices[y * (width + 1) + x] = new Vector3(x, heightValue * _maxHeight, y); // 10f : facteur de hauteur
+            for (int y = 0; y <= height; y++)
+            {
+                for (int x = 0; x <= width; x++)
+                {
+                    float heightValue = _grid[x % width, y % height].height;
+                    vertices[y * (width + 1) + x] =
+                        new Vector3(x, heightValue * _maxHeight, y);
                 }
             }
 
-            // Remplit le tableau des triangles
             int vert = 0;
             int tris = 0;
-            for (int y = 0; y < height; y++) {
-                for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
                     triangles[tris] = vert;
                     triangles[tris + 1] = vert + width + 1;
                     triangles[tris + 2] = vert + 1;
@@ -224,17 +340,17 @@ namespace Local.Noah.Scripts
                     vert++;
                     tris += 6;
                 }
+
                 vert++;
             }
 
-            // Crée un mesh et assigne les sommets et triangles
-            Mesh mesh = new Mesh {
+            Mesh mesh = new Mesh
+            {
                 vertices = vertices,
                 triangles = triangles
             };
-            mesh.RecalculateNormals(); // Recalcule les normales pour l'éclairage
+            mesh.RecalculateNormals();
 
-            // Applique le mesh au MeshFilter
             _meshFilter.sharedMesh = mesh;
         }
     }
