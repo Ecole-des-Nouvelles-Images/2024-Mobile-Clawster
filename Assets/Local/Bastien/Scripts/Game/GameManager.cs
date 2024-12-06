5500deb903844193c868337e7a7a54451efebe6d
Bastien <b.sante@nouvellesimages.xyz>
2024-12-06 12:43:36 +0100

    Skibidi commit

--- Assets/Local/Bastien/Scripts/Game/GameManager.cs
+++ Assets/Local/Bastien/Scripts/Game/GameManager.cs
@@ -8,16 +8,16 @@ public class GameManager : MonoBehaviour {
     public int Level;               //Self-explanatory
     
     [Header("Game Start / End Variables")]
-    public static int CountdownTime;       //Time before game starts
-    public static string StartText;        //Go! or Bin It!
-    public static int EndTime;             //Time before game ends
-    public static string EndText;          //Finish! or Stop!
-    public static int Score;               //Self-explanatory
-    public static int HighScore;           //Hghest score of the session (or the device)
+    public int CountdownTime;       //Time before game starts
+    public string StartText;        //Go! or Bin It!
+    public int EndTime;             //Time before game ends
+    public string EndText;          //Finish! or Stop!
+    public int Score;               //Self-explanatory
+    public int HighScore;           //Highest score of the session (or the device)
     
-    public static bool GameStarted;         //Has the game started ?
+    public bool GameStarted;         //Has the game started ?
 
-    public static List<GameObject> Stack;   //Items transported by Clawster
+    public List<GameObject> Stack;   //Items transported by Clawster
     
     public static GameManager gm;   //SINGLETON BABY
 }
