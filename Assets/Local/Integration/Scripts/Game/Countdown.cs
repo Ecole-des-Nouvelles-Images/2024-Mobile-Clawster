11e6c00c452b513fd26c4e6cc25872faea132211
NoahMil <120110545+NoahMil@users.noreply.github.com>
2024-12-10 01:24:10 +0100

    Adding Stamina System

--- Assets/Local/Integration/Scripts/Game/Countdown.cs
+++ Assets/Local/Integration/Scripts/Game/Countdown.cs
@@ -5,34 +5,46 @@ using UnityEngine;
 
 namespace Local.Integration.Scripts.Game
 {
-    public class Countdown : MonoBehaviour {
+    public class Countdown : MonoBehaviour
+    {
 
         [SerializeField] private TMP_Text _gameTimerText;
         [SerializeField] private TMP_Text _scoreText;
         [SerializeField] private AnimationCurve _easeInOut;
-    
+
         public Vector3 InitialScale;
 
-        private IEnumerator CountdownCoroutine(int time, string str) {
-            if (time >= 1) 
+        private IEnumerator CountdownCoroutine(int time, string str)
+        {
+            if (time > 0)
+            {
                 _gameTimerText.text = time.ToString();
+            }
             else
+            {
                 _gameTimerText.text = str;
-        
+            }
+
             _gameTimerText.transform.localScale = InitialScale;
             _gameTimerText.transform.DOScale(Vector3.one, 1.1f).SetEase(_easeInOut);
 
-            if (time == 0) {
+            yield return new WaitForSeconds(1.1f);
+
+            if (time == 0)
+            {
+                yield return new WaitForSeconds(0.01f); 
+                _gameTimerText.enabled = false;
+                _scoreText.enabled = false;
                 GameManager.instance.HasStarted = true;
-                Debug.Log("Game Started");
                 yield break;
             }
-        
-            yield return new WaitForSeconds(1.1f);
+
             time--;
             StartCoroutine(CountdownCoroutine(time, str));
         }
-        private void Start() {
+
+        private void Start()
+        {
             StartCoroutine(CountdownCoroutine(GameManager.instance.CountdownTime, GameManager.instance.StartText));
         }
     }
