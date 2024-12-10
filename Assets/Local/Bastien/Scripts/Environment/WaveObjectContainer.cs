f2ee87bd7bef6f146298cb18caa25f1ee665264b
BastienSANTE <b.sante@nouvellesimages.xyz>
2024-12-10 23:26:38 +0100

    Created scene Game2, modified crab FBX; not yet exported

--- Assets/Local/Bastien/Scripts/Environment/WaveObjectContainer.cs
+++ Assets/Local/Bastien/Scripts/Environment/WaveObjectContainer.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class WaveObjectContainer : MonoBehaviour
 {
+
+    [SerializeField] private GameObject _original;
+    [SerializeField] private int _objectCount;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(CreateObjects());
     }
 
     // Update is called once per frame
@@ -19,5 +22,15 @@ public class WaveObjectContainer : MonoBehaviour
     public void DropChildren() {
         this.transform.DetachChildren();
         Debug.Log("I lost the kids!");
+    }
+
+    private IEnumerator CreateObjects()
+    {
+        for (int i = 0; i < _objectCount; i++)
+        {
+            Instantiate(_original, this.transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(2f);
+        }
+        
     }
 }
