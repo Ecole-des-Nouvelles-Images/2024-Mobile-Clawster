79418905e5d0d27f1cdd128af49a04fac2cf2d67
NoahMil <noah.miliani13@gmail.com>
2024-12-17 09:52:22 +0100

    Pushing before merge

--- Assets/Local/Integration/Scripts/Game/StaminaUI.cs
+++ Assets/Local/Integration/Scripts/Game/StaminaUI.cs
@@ -4,26 +4,31 @@ namespace Local.Integration.Scripts.Game
 {
     public class StaminaUI : MonoBehaviour
     {
-        [SerializeField] private Transform _playerTransform;
-        [SerializeField] private Vector3 _offset;
-        [SerializeField] private Camera _mainCamera;
+        [SerializeField] private Transform _playerTransform; 
+        [SerializeField] private Vector3 _offset; 
+        [SerializeField] private Canvas _canvas; 
 
-        private RectTransform rectTransform;
+        private RectTransform _rectTransform;
 
         private void Start()
         {
-            rectTransform = GetComponent<RectTransform>();
-            if (_mainCamera == null)
-            {
-                _mainCamera = Camera.main;
-            }
+            _rectTransform = GetComponent<RectTransform>();
         }
 
         private void LateUpdate()
         {
-            if (_playerTransform != null && _mainCamera != null)
+            if (_playerTransform != null && _canvas != null)
             {
-                rectTransform.position = _playerTransform.position + _offset;
+                Vector3 screenPosition = Camera.main.WorldToScreenPoint(_playerTransform.position + _offset);
+
+                RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                    _canvas.GetComponent<RectTransform>(), 
+                    screenPosition, 
+                    _canvas.worldCamera, 
+                    out Vector2 localPosition
+                );
+
+                _rectTransform.localPosition = localPosition;
             }
         }
     }
